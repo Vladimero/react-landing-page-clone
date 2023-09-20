75b8a4852e3ba2868a7a42b45932caa42b1fa7c0
Vladimir Vorozbit <vorozbitcrypto7@gmail.com>
2023-09-20 17:50:16 +0200

    Insert other elements with styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import styles from './App.module.scss';
-import heroImage from './images/heroImage.png';
+import footerImage from './images/footerImage.png';
+import aiImage from './images/heroImage.png';
 import { ReactComponent as Logo } from './images/Logo.svg';
+import macInterface from './images/macInterface.png';
+import { ReactComponent as Direction } from './images/mainButtonDirection.svg';
+import { ReactComponent as BainCompanyLogo } from './images/partner/bainCompanyLogo.svg';
+import { ReactComponent as CreditSuisseLogo } from './images/partner/creditSuisseLogo.svg';
+import { ReactComponent as GartnerLogo } from './images/partner/gartnerLogo.svg';
+import { ReactComponent as HarvardLogo } from './images/partner/harvardLogo.svg';
+import { ReactComponent as McKinseyLogo } from './images/partner/mcKinseyLogo.svg';
+import { ReactComponent as MorgenStanleyLogo } from './images/partner/morgenStanleyLogo.svg';
+import { ReactComponent as MorningStartLogo } from './images/partner/morningStartLogo.svg';
+import { ReactComponent as TheNewYorkTimesLogo } from './images/partner/theNewYorkTimesLogo.svg';
 
 export default function App() {
   return (
@@ -32,22 +43,69 @@ export default function App() {
           </div>
 
           <div className={styles.container}>
-            <div>
-              <h1>Your</h1>
-              <img src={heroImage} alt="Image cannot be loaded" />
-            </div>
-            <div>
-              <h1>knowledge assistant</h1>
+            <div className={styles.heroSection}>
+              <div className={styles.imageConnect}>
+                <h1 className={styles.heroH1}>Your</h1>
+                <div className={styles.imageContainer}>
+                  <img
+                    src={aiImage}
+                    alt="Image cannot be loaded"
+                    className={styles.heroImage}
+                  />
+                </div>
+              </div>
+              <div>
+                <h1 className={styles.heroH1}>knowledge assistant</h1>
+              </div>
             </div>
 
-            <div>
-              <p>
-                Get ready-to-use answers from all your knowledge and quit manual
-                organization for good.
+            <div className={styles.subHeroSection}>
+              <p className={styles.subHeroText}>
+                Get ready-to-use answers from all your knowledge <br /> and quit
+                manual organization for good.
               </p>
+              <div className={styles.heroButton}>
+                <a href="/#" alt="not shown" className={styles.buttonBorder}>
+                  GET STARTED FOR FREE
+                  <Direction />
+                </a>
+              </div>
+            </div>
+
+            <div className={styles.imageBackground}>
+              <img
+                src={macInterface}
+                alt="Image cannot be loaded"
+                className={styles.hugeImage}
+              />
+              <p className={styles.introducePTag}>
+                Loved by professionals from <br /> companies such as
+              </p>
+            </div>
+
+            <div className={styles.logoList}>
+              <BainCompanyLogo />
+              <CreditSuisseLogo />
+              <GartnerLogo />
+              <HarvardLogo />
+              <McKinseyLogo />
+              <MorgenStanleyLogo />
+              <MorningStartLogo />
+              <TheNewYorkTimesLogo />
+              <div className={styles.emptyPlace}>
+                <div className={styles.blur}>
+                  <img
+                    src={footerImage}
+                    alt="not loaded"
+                    className={styles.headerFooterImage}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </section>
+
+        <section></section>
       </main>
     </>
   );
